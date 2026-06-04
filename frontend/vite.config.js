b381b249d0976d9c8b6cd1df164eb9a4d5fc449f
Muzamil hussain <muzamilhussain55441@gmail.com>
2026-06-04 17:55:15 +0500

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://team-task-manager-production-7cb8.up.railway.app/',
         changeOrigin: true,
       }
     }
---
+++ start.sh
@@ -0,0 +1,5 @@
+cd backend
+npm install
+npm run build  # if you have build step (optional)
+
+npm start
