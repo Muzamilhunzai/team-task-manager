cab6e559a5611f8d68d88694550d0338dbc94866
Muzamil hussain <muzamilhussain55441@gmail.com>
2026-06-04 18:59:10 +0500

    updates start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-=======
 #!/bin/bash
+set -e
 
->>>>>>> 619e2ce8569af58cd5c6e7179d406e76af62c840
 cd backend
-npm install
-npm run build  # if you have build step (optional)
 
-<<<<<<< HEAD
+echo "Installing dependencies..."
+npm install
+
+echo "Starting server..."
 npm start
-=======
-npm start
->>>>>>> 619e2ce8569af58cd5c6e7179d406e76af62c840
